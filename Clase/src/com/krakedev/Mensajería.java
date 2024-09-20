1abf51403fd06e7dac6ae5cb4637adc1f9d69ad3
Hidalgo369 <ljhidalboy@gmail.com>
2024-09-20 15:56:24 -0500

    Práctica con métodos, mensajería

---
+++ Clase/src/com/krakedev/Mensajería.java
@@ -0,0 +1,11 @@
+package com.krakedev;
+
+public class Mensajería {
+	public void saludar() {
+		System.out.println("Hola Mundo!");
+	}
+	
+	public void saludarAmigo(String nombre,String apellido) {
+		System.out.println("Hola " + nombre + " " + apellido);
+	}
+}
---
+++ Clase/src/com/krakedev/TestMensajería.java
@@ -0,0 +1,11 @@
+package com.krakedev;
+
+public class TestMensajería {
+
+	public static void main(String[] args) {
+		Mensajería m = new Mensajería();
+		m.saludar();
+		m.saludarAmigo("Leython", "Hidalgo");
+	}
+
+}
