43511866f03f4032fe82126eb87781e92dcaaa32
Hidalgo369 <ljhidalboy@gmail.com>
2024-09-20 15:58:35 -0500

    Ejercicio de calculadora con función de suma y resta

---
+++ Clase/src/com/krakedev/TestCalculadora.java
@@ -0,0 +1,15 @@
+package com.krakedev;
+
+public class TestCalculadora {
+
+	public static void main(String[] args) {
+		Calculadora calcu = new Calculadora();
+		int resultadoSuma = calcu.sumar(5, 8);
+		double resultadoResta = calcu.restar(10, 3);
+		
+		System.out.println("RESULTADO SUMA: " + resultadoSuma);
+		System.out.println("RESULTADO RESTA: " + resultadoResta);
+		
+	}
+
+}
