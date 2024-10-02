26a2049208587a5ff0ed092f2ed3d1a28b0332d8
Hidalgo369 <ljhidalboy@gmail.com>
2024-10-02 14:09:24 -0500

    Configurando la librería junit y explicación del proyecto realizado

---
+++ ejemplojunit/src/main/java/com/krakedev/ejemplojunit/Calculadora.java
@@ -0,0 +1,12 @@
+package com.krakedev.ejemplojunit;
+
+public class Calculadora {
+	public int Sumar(int valor1, int valor2) {
+		return valor1 + valor2;
+	}
+
+	public int Restar(int valor1, int valor2) {
+		return valor1 - valor2;
+	}
+	
+}
